6ee622c7247d6a0590968b3fe8fb1df7fa80fd35
all-tool-vip <88178692+all-tool-vip@users.noreply.github.com>
2021-07-30 12:44:50 +0700

    Add files via upload

---
+++ tool-vip.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+clear
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+echo -e $HIJAU
+sleep 1
+echo "============================="
+echo "[+] AUTHOR : TOOL-VIP"
+echo "[+] TEAM : VIP"
+echo "=========================================================="
+echo "1). HACKING FACEBOOK (Only one time hack)"
+echo "2). HACKING WHATSAPP (pay 20 dollars)"
+echo "3). HACKING WHATSAPP (2 days free trial)"
+echo "4). HACKING Hack in one wifi network (2 days free trial)"
+echo "=========================================================="
+read -p "=>: " dont
+
+if [  $dont = 1 ]
+then
+read -p "Enter target number : " nomor;
+echo -e $MERAH
+echo "error 404 forbidden"
+fi
+
+if [ $dont = 2 ]
+then
+echo "Mau Pesan Tool Hacking real : 6285871424841"
+fi
+
+if [ $dont = 3 ]
+then
+read -p "victim number:"
+echo -e $MERAH
+echo "404 whatsapp forbidden to enter our system"
+fi
+
+if [ $dont = 4 ]
+then
+read -p "Enter WIFI NETWORK SSID:"
+sleep 5
+echo "https://google.com"
+sleep 1
+echo "https://facebook.com"
+echo" : login facebook from wifi"
+echo " username: budisetiawan19@gmail.com"
+echo " password: budiset19"
+sleep 1
+echo "https://youtube.com"
+termux-setup-storage
+fi
